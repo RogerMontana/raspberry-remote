0f2f083a6faf2021148e4f209a737f639276752a
Artem K <artem.karpov@yahoo.com>
2020-04-07 14:39:34 +0300

    fix display cmd

--- server/server.py
+++ server/server.py
@@ -31,7 +31,7 @@ class CovidInfoHandler(tornado.web.RequestHandler):
         by_countries = country_summary['Countries']
         for country_info in by_countries:
             if country_info['CountryCode'] == countryCode:
-                info = "Covid{} Deaths:{} NewConf:{} Total:{}".format(country_info['Country'],
+                info = "Covid19_Stats Country:{} Deaths:{} NewConf:{} Total:{}".format(country_info['Country'],
                                                                     country_info['TotalDeaths'],
                                                                     country_info['NewConfirmed'],
                                                                     country_info['TotalConfirmed'])
